419ee38ec2a00911e6c3dcba8f535e2d4a03fbfc
soso <soumiaelhali@gmail.com>
2020-07-19 18:50:49 +0100

    first commit

---
+++ src/main/java/com/example/ExamenSpringBoot/ExamenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSpringBootApplication.class, args);
+	}
+
+}
